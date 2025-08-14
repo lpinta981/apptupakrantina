2a8f83bd9239d513b36c7fdbbffe0dc251cb1602
lpinta981 <108894335+lpinta981@users.noreply.github.com>
2025-08-14 17:13:05 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 // Credenciales para la conexión con tu proyecto de Supabase.
 const SUPABASE_URL = 'https://lpsupabase.luispinta.com'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzE1MDUwODAwLAogICJleHAiOiAxODcyODE3MjAwCn0.88fV2pgv-28a5uvuiD-boVVW6m5AFRhhd_DooiWJ_Pc';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzE1MDUwODAwLAogICJleHAiOiAxODcyODE3MjAwCn0.c9lDffnJrU-JBqtYYh8WgUpEaN4HoUk5ecnZgBKX8OA';
 
 // Inicializa el cliente de Supabase. No es necesario modificar esta línea.
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
